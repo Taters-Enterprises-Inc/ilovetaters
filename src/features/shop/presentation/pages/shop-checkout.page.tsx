fe6e910df3fb4ccba774125dbb63861de6c38968
ecoknows <jerico.villaraza19@gmail.com>
2022-08-12 12:00:23 +0800

    :construction: work in progress checkout

--- src/features/shop/presentation/pages/shop-checkout.page.tsx
+++ src/features/shop/presentation/pages/shop-checkout.page.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { ShopHeaderNav } from "../header/shop-header-nav.component";
 import { Input } from "@material-tailwind/react";
 import { FooterNav } from "features/shared";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdDeliveryDining } from "react-icons/md";
+import { FaMapMarkerAlt, FaStore } from "react-icons/fa";
 
 export function ShopCheckout(){
     return (
@@ -15,7 +18,7 @@ export function ShopCheckout(){
                 
             <section className="min-h-screen container mx-auto lg:space-x-4 pb-36">
                 
-                <div className="lg:-mt-[180px] lg:space-y-8">
+                <div className="lg:-mt-[200px] lg:space-y-8">
 
                     <div className="px-4 py-6 lg:py-0 flex flex-col lg:flex-row justify-between items-center bg-secondary space-y-2">
                         <h1 className="text-white font-['Bebas_Neue'] tracking-[3px] text-3xl leading-8">Checkout</h1>
@@ -24,7 +27,7 @@ export function ShopCheckout(){
 
                             <ol className="inline-flex items-center space-x-1 md:space-x-3">
                                 <li className="inline-flex items-center">
-                                    <Link to='/shop' className="inline-flex items-center text-xs lg:text-base font-medium text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white">
+                                    <Link to='/shop' className="inline-flex items-center text-xs lg:text-base font-medium text-gray-700 dark:text-white dark:hover:text-white">
                                         <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
                                         Snackshop
                                     </Link>
@@ -32,13 +35,13 @@ export function ShopCheckout(){
                                 <li>
                                 <div className="flex items-center">
                                     <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                                    <Link to='/shop/products' className="ml-1 text-xs lg:text-base font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-white dark:hover:text-white">Products</Link>
+                                    <Link to='/shop/products' className="ml-1 text-xs lg:text-base font-medium text-gray-700 md:ml-2 dark:text-white dark:hover:text-white">Products</Link>
                                 </div>
                                 </li>
                                 <li aria-current="page">
                                 <div className="flex items-center">
                                     <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                                    <span className="ml-1 text-xs lg:text-base font-medium text-gray-400 md:ml-2 ">Checkout</span>
+                                    <span className="ml-1 text-xs lg:text-base font-medium text-white md:ml-2 ">Checkout</span>
                                 </div>
                                 </li>
                             </ol>
@@ -52,7 +55,7 @@ export function ShopCheckout(){
                             <div className="bg-white h-[0.25rem] relative">
                                 <div className="absolute rounded-[50%] bg-white font-bold h-[1.625rem] w-[1.625rem] text-center top-[-0.75rem] left-[50%] ml-[-0.8125rem]">1</div>
                             </div>
-                            <div className="flex justify-center items-center mt-5 text-xs font-bold text-white">
+                            <div className="flex justify-center items-center mt-5 text-xs text-white">
                                 Your Details
                             </div>
                         </div>
@@ -61,7 +64,7 @@ export function ShopCheckout(){
                             <div className="bg-[#424242] h-[0.25rem] relative">
                                 <div className="absolute rounded-[50%] text-white font-bold bg-[#424242] h-[1.625rem] w-[1.625rem] text-center top-[-0.75rem] left-[50%] ml-[-0.8125rem]">2</div>
                             </div>
-                            <div className="flex justify-center items-center mt-5 text-xs font-bold text-white">
+                            <div className="flex justify-center items-center mt-5 text-xs text-white">
                                 Payment
                             </div>
                         </div>
@@ -71,7 +74,7 @@ export function ShopCheckout(){
                             <div className="bg-[#424242] h-[0.25rem] relative">
                                 <div className="absolute rounded-[50%] text-white font-bold bg-[#424242] h-[1.625rem] w-[1.625rem] text-center top-[-0.75rem] left-[50%] ml-[-0.8125rem]">3</div>
                             </div>
-                            <div className="flex justify-center items-center mt-5 text-xs font-bold text-white">
+                            <div className="flex justify-center items-center mt-5 text-xs text-white">
                                 Complete
                             </div>
                         </div>
@@ -79,16 +82,74 @@ export function ShopCheckout(){
                     </div>
                     
 
-                    <div className="bg-primary py-6 px-4 lg:shadow-[#540808] lg:shadow-md w-full lg:rounded-[30px] mb-10 lg:p-10 flex lg:space-x-10 space-y-10 lg:space-y-0 flex-col lg:flex-row">
-                        <div className="space-y-4">
+                    <div className="bg-primary py-6 px-4 lg:shadow-[#540808] lg:shadow-md w-full lg:rounded-[30px] mb-10 lg:p-10 flex justify-between flex-col lg:flex-row">
+                        
+                        <div className="space-y-4 lg:flex-[0_0_55%] lg:max-w-[55%] order-2 lg:order-1 lg:mt-0 mt-4">
+                            <Input variant="outlined" color="orange" size="lg" label="First Name"/>
+                            <Input variant="outlined" color="orange" size="lg" label="Last Name"/>
+                            <div className="flex lg:space-x-4 flex-col lg:flex-row space-y-4 lg:space-y-0">
+                                <div className="flex-1">
+                                    <Input variant="outlined" color="orange" size="lg" label="E-mail Address" />
+                                </div>
+                                <div className="flex-1">
+                                    <Input variant="outlined" color="orange" size="lg" label="Phone Number"/>
+                                    <span className="text-xs text-tertiary underline underline-offset-4">Setup your phone number</span>
+                                </div>
+                            </div>
+                            
+
+                            <div className="text-white order-last lg:mt-0 mt-4">
+                                <h2 className="text-2xl font-['Bebas_Neue'] tracking-[2px]">Handling Method</h2>
+
+                                <ul className="space-y-1 mt-2">
+                                    <li className="flex space-x-2 items-center">
+                                        <MdDeliveryDining className="text-2xl text-tertiary"/>
+                                        <h3 className="text-sm">Delivery</h3>
+                                    </li>
+                                    <li className="flex space-x-3 items-start">
+                                        <FaStore className="text-lg text-tertiary"/>
+                                        <h3 className="text-sm">Store: Taters Robinsons Magnolia</h3>
+                                    </li>
+                                    <li className="flex space-x-3 items-start ">
+                                        <FaMapMarkerAlt className="text-lg text-tertiary"/>
+                                        <h3 className="text-sm flex-1">Store Address: 3rd Level, Robinsons Movieworld Magnolia Town Center, Brgy Kaunlara, Quezon City</h3>
+                                    </li>
+                                </ul>
+                            </div>
+
+
+
+                            <div className="text-white order-last lg:mt-0 mt-4">
+                                <h2 className="text-2xl font-['Bebas_Neue'] tracking-[2px]">Note: </h2>
+
+                                <ul className="space-y-2 mt-2">
+                                    <li>
+                                        <h3 className="text-sm">Delivery and/or Pick-up of items are from Monday to Sunday (except holidays) between 11AM to 7PM</h3>
+                                    </li>
+                                    <li>
+                                        <h3 className="text-sm">Delivery and/or Pick-up of product would be on the same day if paid before 5:00 pm</h3>
+                                    </li>
+                                    <li>
+                                        <h3 className="text-sm">You will be charged with a delivery fee depending on your location</h3>
+                                    </li>
+                                    <li>
+                                        <h3 className="text-sm">Our stores will reach out to you via SMS once orders are ready for delivery/pick-up</h3>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                        <div className="space-y-4 lg:flex-[0_0_40%] lg:max-w-[40%] order-1 lg:order-2">
                             <h2 className="font-['Bebas_Neue'] text-3xl  text-white tracking-[3px] text-center">Order Summary</h2>
 
                             <div className="flex bg-secondary shadow-md shadow-tertiary rounded-[10px]">
                                 <img src={REACT_APP_DOMAIN_URL + "uploads/images/shop/products/100/test.jpg"} className="rounded-[10px] w-[92px] h-[92px]" alt="" />
-                                <div className="flex-1 text-white px-3 py-2">
-                                    <h3 className="text-base">Family Pack Tofu Chips x 1</h3>
-                                    <h3 className="text-base">Flavor: <span className="text-tertiary">Natural</span></h3>
-                                    <h3 className="text-lg text-end">₱ 295.00</h3>
+                                <div className="flex-1 text-white px-3 py-2 flex flex-col">
+                                    <h3 className="text-sm">Family Pack Tofu Chips</h3>
+                                    <h3 className="text-xs">Quntity: <span className="text-tertiary">3</span></h3>
+                                    <h3 className="text-xs">Flavor: <span className="text-tertiary">Natural</span></h3>
+                                    <h3 className="text-base flex-1 flex justify-end items-end">₱ 295.00</h3>
                                 </div>
                             </div>
                             <hr className="mt-1" />
@@ -103,12 +164,6 @@ export function ShopCheckout(){
                             </div>
 
                             <h1 className="text-4xl text-center text-white">₱ 295.00</h1>
-                            
-                            <Input variant="outlined" color="orange" size="lg" label="First Name"/>
-                            <Input variant="outlined" color="orange" size="lg" label="Last Name"/>
-                            <Input variant="outlined" color="orange" size="lg" label="E-mail Address"/>
-                            <Input variant="outlined" color="orange" size="lg" label="Phone Number"/>
-                            
                         </div>
                     </div>
                 </div>
--- src/features/shop/presentation/pages/shop-product.page.tsx
+++ src/features/shop/presentation/pages/shop-product.page.tsx
@@ -28,22 +28,22 @@ export function ShopProduct(){
 
                             <ol className="inline-flex items-center space-x-1 md:space-x-3">
                                 <li className="inline-flex items-center">
-                                    <Link to='/shop' className="inline-flex items-center text-xs lg:text-base font-medium text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white">
+                                    <Link to='/shop' className="inline-flex items-center text-xs lg:text-base font-medium text-gray-700 dark:text-white dark:hover:text-white">
                                         <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
                                         Snackshop
                                     </Link>
                                 </li>
                                 <li>
-                                <div className="flex items-center">
-                                    <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                                    <Link to='/shop/products' className="ml-1 text-xs lg:text-base font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-white dark:hover:text-white">Products</Link>
-                                </div>
+                                    <div className="flex items-center">
+                                        <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                        <Link to='/shop/products' className="ml-1 text-xs lg:text-base font-medium text-gray-700 md:ml-2 dark:text-white dark:hover:text-white">Products</Link>
+                                    </div>
                                 </li>
                                 <li aria-current="page">
-                                <div className="flex items-center">
-                                    <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                                    <span className="ml-1 text-xs lg:text-base font-medium text-gray-400 md:ml-2 ">Lorem ipsum dolor sit amet</span>
-                                </div>
+                                    <div className="flex items-center">
+                                        <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                        <span className="ml-1 text-xs lg:text-base font-medium text-white md:ml-2 whitespace-nowrap overflow-hidden lg:max-w-full max-w-[80px] text-ellipsis">Lorem ipsum dolor sit amet</span>
+                                    </div>
                                 </li>
                             </ol>
 
@@ -106,8 +106,8 @@ export function ShopProduct(){
                                     <div className="p-4 flex space-x-2">
                                         <img src={REACT_APP_DOMAIN_URL + "uploads/images/shop/products/100/test.jpg"} className="rounded-[10px] w-[100px] h-[100px]" alt="" />
                                         <div className="p-2 space-y-2">
-                                            <h4 className="font-['Bebas_Neue'] text-xl tracking-[2px] leading-5">Taters Snackstix</h4>
-                                            <h5 className="text-lg font-bold text-tertiary leading-5">₱ 50.00 x 1 = ₱ 50.00</h5>
+                                            <h4 className="font-['Bebas_Neue'] text-lg tracking-[2px] leading-5">Taters Snackstix</h4>
+                                            <h5 className=" text-tertiary leading-5">₱ 50.00</h5>
                                             <CounterInput/>
 
                                         </div>
@@ -143,13 +143,13 @@ export function ShopProduct(){
                                     <div className="flex flex-row h-full w-full rounded-lg relative bg-transparent mt-1 border-2 border-white text-white">
 
                                         <button className=" h-full w-[150px] rounded-l cursor-pointer outline-none bg-primary">
-                                            <span className="m-auto text-5xl font-thin leading-3">−</span>
+                                            <span className="m-auto text-5xl font-thin">−</span>
                                         </button>
 
                                         <input type="number" className="text-3xl leading-2 bg-secondary outline-none text-center w-full font-semibold text-md  md:text-basecursor-default flex items-center" name="custom-input-number" value="0"/>
                                         
                                         <button className="h-full w-[150px] rounded-r cursor-pointer bg-primary">
-                                            <span className="m-auto text-5xl font-thin leading-3">+</span>
+                                            <span className="m-auto text-5xl font-thin">+</span>
                                         </button>
 
                                     </div>
@@ -158,11 +158,11 @@ export function ShopProduct(){
                             </div>
 
 
-                            <h2 className="text-5xl text-white font-bold mt-4">₱ 250.00</h2>
+                            <h2 className="text-4xl text-white mt-4">₱ 250.00</h2>
 
-                            <button className="text-white text-xl flex space-x-6 justify-center items-center bg-[#CC5801] py-4 w-full rounded-xl ">
-                                <BsFillCartPlusFill className="text-4xl"/>
-                                <span className="text-3xl font-['Bebas_Neue'] tracking-[3px] font-light mt-1">Add to cart</span>
+                            <button className="text-white text-xl flex space-x-2 justify-center items-center bg-[#CC5801] py-4 w-full rounded-xl ">
+                                <BsFillCartPlusFill className="text-3xl"/>
+                                <span className="text-2xl font-['Bebas_Neue'] tracking-[3px] font-light mt-1">Add to cart</span>
                             </button>
                             
                         </div>
