f2ef72b5e67431b8403a7a1af53402ccbe7d7590
unknown <yanceevillanueva16@gmail.com>
2022-08-24 20:51:07 +0800

    sidebar changes

--- src/features/admin/presentation/pages/admin.page.tsx
+++ src/features/admin/presentation/pages/admin.page.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {FaBars, FaRegListAlt} from 'react-icons/fa';
 
-// const [open, setOpen] = useState (true);
-
 export function Admin(){
+    const [open, setOpen] = useState (true);
+
     return(
         <div className='flex'>
-            <div className='bg-primary h-screen w-64 relative'>
-            <FaBars className='relative cursor-pointer text-white top-4 -right-56'></FaBars>
+            <div className={'h-screen relative bg-primary'} >
+            <FaBars className='relative cursor-pointer text-white top-4 -right-56' onClick={() => setOpen(!open)}></FaBars>
             </div>
             <div className='bg-red-100 h-screen p-3 flex-1'>
                 <h1>Orders</h1>
@@ -15,8 +15,5 @@ export function Admin(){
 
         </div>
 
-        // <section className='bg-red-100 h-screen'>
-        //     <h1>Orders</h1>
-        // </section>
     );
 }
