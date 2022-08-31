2b83b84cd3fcbd4473f72f67eff56eaa14a5e6e3
unknown <yanceevillanueva16@gmail.com>
2022-08-31 14:54:44 +0800

    sidebar updates

--- src/features/admin/presentation/components/sidebar.tsx
+++ src/features/admin/presentation/components/sidebar.tsx
@@ -37,14 +37,14 @@ export function Sidebar() {
         </div>
 
         <div
-          className={`whitespace-pre duration-300 mt-4 text-white 
+          className={`whitespace-pre duration-300 mt-3 text-white 
                 ${!open && "opacity-0 translate-x-28 overflow-hidden "}`}
         >
           <h3 className="cursor-pointer">Administrator</h3>
           <h4 className="text-xs cursor-pointer">Admin, Members</h4>
         </div>
 
-        <div className="relative flex flex-col pb-4 mt-4 text-sm text-white">
+        <div className="relative flex flex-col pb-4 mt-3 text-sm text-white">
           {Menus?.map((menu, i) => (
             <Link
               to={menu?.link}
--- src/features/admin/presentation/pages/admin.page.tsx
+++ src/features/admin/presentation/pages/admin.page.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { Sidebar } from "../components/sidebar";
+import { Head } from "../components/head";
 
 export function Admin() {
   return (
+    // dont change code here
     <div className="flex ">
       <div className="overflow-y-auto">
         <Sidebar />
       </div>
       <div className="flex-1 min-h-screen p-2 overflow-y-hidden text-white bg-secondary">
-        <h1>Orders</h1>
+        <Head />
+
+        {/* change page info here */}
+        <h1 className="font-['Bebas_Neue'] text-3xl ml-4 mt-5">Orders</h1>
       </div>
     </div>
   );
