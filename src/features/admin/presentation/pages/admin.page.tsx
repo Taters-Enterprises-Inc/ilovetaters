e2ad6befb8daad51a8ee697f5cdf37a6f80be879
unknown <yanceevillanueva16@gmail.com>
2022-08-31 12:46:49 +0800

    fix

--- src/features/admin/presentation/pages/admin.page.tsx
+++ src/features/admin/presentation/pages/admin.page.tsx
@@ -5,7 +5,7 @@ export function Admin() {
   return (
     <div className="flex ">
       <Sidebar />
-      <div className="flex-1 min-h-screen p-2 overflow-y-hidden bg-red-100">
+      <div className="flex-1 min-h-screen p-2 overflow-y-hidden bg-secondary">
         <h1>Orders</h1>
       </div>
     </div>
