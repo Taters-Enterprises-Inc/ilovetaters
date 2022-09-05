391c6de144a6a107e4b367a202a38a25488d52e0
knhrvs <knhrvs.dev@gmail.com>
2022-09-05 13:32:13 +0800

    🛠️ Fix Issue #45

--- src/features/home/presentation/pages/home.page.tsx
+++ src/features/home/presentation/pages/home.page.tsx
@@ -59,7 +59,7 @@ export function Home() {
           backgroundRepeat: "no-repeat",
           backgroundAttachment: "fixed",
         }}
-        className="relative items-end justify-center hidden sm:flex "
+        className="relative items-end justify-center hidden filter saturate-150 contrast-100 brightness-75 sm:flex "
       >
         <img
           src={
