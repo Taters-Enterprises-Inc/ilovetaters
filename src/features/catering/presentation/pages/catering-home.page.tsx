5dcb4c6cc2a00d9290cf7a96ab4fe0fc359c2b9e
knhrvs <knhrvs.dev@gmail.com>
2022-09-17 01:52:28 +0800

    Revert "🚧 Work in progress"
    This reverts commit 9dcd0b757b64fc5a956f2f1b1f2730ff2e7f9a66.

--- src/features/catering/presentation/pages/catering-home.page.tsx
+++ src/features/catering/presentation/pages/catering-home.page.tsx
@@ -104,8 +104,10 @@ export function CateringHome() {
       </section>
       <section className="container pb-96">
         <h1 className='text-white text-lg pt-4 pb-2 font-["Bebas_Neue"] tracking-[2px] text-center leading-tight'>
-          Thank you for considering Taters for your celebration. <span className='block lg:inline'> Kindly key in
-          your event details. </span>
+          Thank you for considering Taters for your celebration.
+          <span className="block lg:inline">
+            Kindly key in your event details.
+          </span>
         </h1>
 
         <div className="space-y-4">
@@ -239,6 +241,23 @@ export function CateringHome() {
               if (
                 cateringHomePageState.eventStartDate >=
                 cateringHomePageState.eventEndDate
+              ) {
+                dispatch(
+                  popUpSnackBar({
+                    message: "Please select valid end date",
+                    severity: "error",
+                  })
+                );
+                return;
+              }
+
+              if (
+                moment(cateringHomePageState.eventStartDate)
+                  .add(3, "hours")
+                  .format("YYYY-MM-DD hh:mm") >=
+                moment(cateringHomePageState.eventEndDate).format(
+                  "YYYY-MM-DD hh:mm"
+                )
               ) {
                 dispatch(
                   popUpSnackBar({
