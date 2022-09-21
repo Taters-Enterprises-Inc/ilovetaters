7b6d6f2a4bf0d53bbf0a46aa093e3f0adb4297af
dwinurbano <godwunurbano07@gmail.com>
2022-09-22 01:33:10 +0800

    Css modified.

--- src/features/admin/presentation/pages/admin-faqs.page.tsx
+++ src/features/admin/presentation/pages/admin-faqs.page.tsx
@@ -14,11 +14,11 @@ export function AdminFaq() {
   return (
     <>
       {/* change page info here */}
-      <div className="relative block h-screen overflow-y-auto">
+      <div className="relative block h-screen">
         <h1 className="font-['Bebas_Neue'] text-3xl ml-4 mt-5 text-secondary max-h-screen">
           Frequently Asked Questions
         </h1>
-        <main className="min-h-screen bg-paper">
+        <main className="container block pb-4 min-h-min">
           <section className="container py-6">
             <Tab tabs={ADMIN_FAQ} activeTab="store">
               <CustomizedAccordionsSFAQ></CustomizedAccordionsSFAQ>
