b472ed7f0e0f729694ba3185ef01542d4d3a3166
ecoknows <eco.villaraza@gmail.com>
2022-10-12 07:46:58 +0800

    :bug: fix shop audit

--- src/features/admin/presentation/components/admin-shop-order-audit.tsx
+++ src/features/admin/presentation/components/admin-shop-order-audit.tsx
@@ -30,7 +30,7 @@ export function AdminShopOrderAudit() {
 
   return (
     <div>
-      <table className="hidden w-full my-3 text-sm text-left rounded-lg lg:table customer-information-table">
+      <table className="hidden w-full mt-3 text-sm text-left rounded-lg lg:table customer-information-table">
         <thead className="text-xs text-white uppercase bg-secondary ">
           <tr>
             <th scope="col" className="px-6 py-3">
