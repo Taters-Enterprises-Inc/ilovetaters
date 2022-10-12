37670f122dc9c1b8fcee1c6b7873f90277732054
ecoknows <eco.villaraza@gmail.com>
2022-10-12 14:07:13 +0800

    :construction: in progress notification

--- src/features/shared/presentation/components/admin-notification.wrapper.tsx
+++ src/features/shared/presentation/components/admin-notification.wrapper.tsx
@@ -1,22 +1,44 @@
 import { Outlet } from "react-router-dom";
 import Pusher from "pusher-js";
 import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const pusher = new Pusher("8a62b17c8a9baa690edb", {
   cluster: "ap1",
 });
 
+interface SnackshopSnackshopNotificationParam {
+  store_id: string;
+  message: string;
+}
+
 export function AdminNotificationWrapper() {
   useEffect(() => {
     const snackshopChannel = pusher.subscribe("snackshop");
 
-    snackshopChannel.bind("order-transaction", (data: any) => {
-      // TODO: Notification for order-transaction
-    });
+    snackshopChannel.bind(
+      "order-transaction",
+      (data: SnackshopSnackshopNotificationParam) => {
+        toast(data.message);
+      }
+    );
   }, []);
 
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Outlet />
     </>
   );
