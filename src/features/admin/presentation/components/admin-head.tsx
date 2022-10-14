b23676c6f416747239f997a5b463688b419836a6
ecoknows <jerico.villaraza19@gmail.com>
2022-10-14 17:42:42 +0800

    :bug: add badge for notification

--- src/features/admin/presentation/components/admin-head.tsx
+++ src/features/admin/presentation/components/admin-head.tsx
@@ -1,19 +1,9 @@
-import { useEffect } from "react";
-import { TbLogout } from "react-icons/tb";
 import { MdOutlineNotificationsNone } from "react-icons/md";
-import { useAppDispatch, useAppSelector } from "features/config/hooks";
-import {
-  logoutAdmin,
-  LogoutAdminState,
-  resetLogoutAdmin,
-  selectLogoutAdmin,
-} from "../slices/logout-admin.slice";
-import { getAdminSession } from "../slices/get-admin-session.slice";
 import { AdminBreadCrumbs, AdminBreadCrumbsProps } from "./admin-breadcrumbs";
-import { useNavigate } from "react-router-dom";
 import Popper from "@mui/material/Popper";
 import * as React from "react";
 import Box from "@mui/material/Box";
+import Badge from "@mui/material/Badge";
 
 interface AdminHeadProps {
   AdminBreadCrumbsProps: AdminBreadCrumbsProps;
@@ -28,34 +18,14 @@ export function AdminHead(props: AdminHeadProps) {
 
   const open = Boolean(anchorEl);
   const id = open ? "simple-popper" : undefined;
-
-  const dispatch = useAppDispatch();
-  const logoutAdminState = useAppSelector(selectLogoutAdmin);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (logoutAdminState.status === LogoutAdminState.success) {
-      dispatch(getAdminSession());
-      dispatch(resetLogoutAdmin());
-      navigate("/admin");
-    }
-  }, [logoutAdminState, dispatch]);
-
   return (
     <div className="flex justify-between p-4">
       <AdminBreadCrumbs {...props.AdminBreadCrumbsProps} />
-
       <div className="relative flex items-center justify-end text-secondary ">
-        {/* <MdOutlineNotificationsNone
-          className="mr-4 cursor-pointer "
-          size={20}
-        /> */}
-
         <button aria-describedby={id} type="button" onClick={handleClick}>
-          <MdOutlineNotificationsNone
-            className="mr-2 cursor-pointer "
-            size={20}
-          />
+          <Badge badgeContent={0} color="primary">
+            <MdOutlineNotificationsNone className="cursor-pointer " size={20} />
+          </Badge>
         </button>
 
         <Popper id={id} open={open} anchorEl={anchorEl}>
@@ -86,14 +56,6 @@ export function AdminHead(props: AdminHeadProps) {
             </Box>
           </div>
         </Popper>
-
-        <TbLogout
-          onClick={() => {
-            dispatch(logoutAdmin());
-          }}
-          className="cursor-pointer"
-          size={20}
-        />
       </div>
     </div>
   );
