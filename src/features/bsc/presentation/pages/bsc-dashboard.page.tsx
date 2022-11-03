133be504ba8364ee726b271f6c86e3a7b2bc8d0a
unknown <yanceevillanueva16@gmail.com>
2022-11-03 18:46:58 +0800

    button

--- src/features/bsc/presentation/pages/bsc-dashboard.page.tsx
+++ src/features/bsc/presentation/pages/bsc-dashboard.page.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import { BSCHead } from "../components/bsc-head";
+import { RiSurveyLine } from "react-icons/ri";
 
 export function BscDashboard() {
   return (
@@ -13,6 +15,22 @@ export function BscDashboard() {
           pageTitles: [{ name: "Dashboard", url: "/bsc/dashboard" }],
         }}
       />
+      <div className="px-4">
+        <span className="text-secondary text-3xl font-['Bebas_Neue'] flex-1">
+          DASHBOARD
+        </span>
+        <div className="flex flex-col space-y-1 lg:flex-row lg:space-x-4 lg:space-y-0">
+          <div>
+            <Link
+              to="form"
+              className="inline-flex items-center px-4 tracking-wide py-1  bg-button font-['Varela_Round'] text-white text-xs rounded-md font-700"
+            >
+              <RiSurveyLine size={20} />
+              <span>&nbsp;&nbsp;Go to form</span>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
