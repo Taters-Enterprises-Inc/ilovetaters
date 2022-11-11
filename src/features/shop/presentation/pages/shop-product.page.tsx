664d25edd69a4d8cd81cc7d54330f638939b9432
ecoknows <eco.villaraza@gmail.com>
2022-11-11 12:07:11 +0800

    :ambulance: hotfix add discount on accordion cart

--- src/features/shop/presentation/pages/shop-product.page.tsx
+++ src/features/shop/presentation/pages/shop-product.page.tsx
@@ -534,14 +534,39 @@ export function ShopProduct() {
                               </h3>
                             ) : null}
 
-                            <h3 className="flex items-end justify-end flex-1 text-base">
-                              <NumberFormat
-                                value={order.prod_calc_amount.toFixed(2)}
-                                displayType={"text"}
-                                thousandSeparator={true}
-                                prefix={"₱"}
-                              />
-                            </h3>
+                            {order.promo_discount_percentage ? (
+                              <div>
+                                <h3 className="flex items-end justify-end flex-1 text-sm line-through">
+                                  <NumberFormat
+                                    value={order.prod_calc_amount.toFixed(2)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"₱"}
+                                  />
+                                </h3>
+                                <h3 className="flex items-end justify-end flex-1 text-base">
+                                  <NumberFormat
+                                    value={(
+                                      order.prod_calc_amount -
+                                      order.prod_calc_amount *
+                                        order.promo_discount_percentage
+                                    ).toFixed(2)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"₱"}
+                                  />
+                                </h3>
+                              </div>
+                            ) : (
+                              <h3 className="flex items-end justify-end flex-1 text-base">
+                                <NumberFormat
+                                  value={order.prod_calc_amount.toFixed(2)}
+                                  displayType={"text"}
+                                  thousandSeparator={true}
+                                  prefix={"₱"}
+                                />
+                              </h3>
+                            )}
                           </div>
                           <button
                             className="absolute text-white top-2 right-4 "
