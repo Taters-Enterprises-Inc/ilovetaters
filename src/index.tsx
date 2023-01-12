cc711b72e112431465ada858a2b2521c3da18856
ecoknows <jerico.villaraza19@gmail.com>
2023-01-12 16:20:35 +0800

    :ambulance: hotfix admin avaliabilities accordion

--- src/index.tsx
+++ src/index.tsx
@@ -231,32 +231,38 @@ root.render(
                               <Route path="store" element={<AdminFaq />} />
                               <Route path="customer" element={<AdminCFaq />} />
                             </Route>
-
                             <Route path="availability">
-                              <Route
-                                path="deal"
-                                element={<AdminAvailabilityDeal />}
-                              />
-                              <Route
-                                path="product"
-                                element={<AdminAvailabilityProduct />}
-                              />
-                              <Route
-                                path="caters-package"
-                                element={<AdminAvailabilityCatersPackage />}
-                              />
-                              <Route
-                                path="caters-package-addon"
-                                element={
-                                  <AdminAvailabilityCatersPackageAddon />
-                                }
-                              />
-                              <Route
-                                path="caters-product-addon"
-                                element={
-                                  <AdminAvailabilityCatersProductAddon />
-                                }
-                              />
+                              <Route path="shop">
+                                <Route
+                                  path="product"
+                                  element={<AdminAvailabilityProduct />}
+                                />
+                              </Route>
+
+                              <Route path="catering">
+                                <Route
+                                  path="package"
+                                  element={<AdminAvailabilityCatersPackage />}
+                                />
+                                <Route
+                                  path="package-addon"
+                                  element={
+                                    <AdminAvailabilityCatersPackageAddon />
+                                  }
+                                />
+                                <Route
+                                  path="product-addon"
+                                  element={
+                                    <AdminAvailabilityCatersProductAddon />
+                                  }
+                                />
+                              </Route>
+                              <Route path="popclub">
+                                <Route
+                                  path="deal"
+                                  element={<AdminAvailabilityDeal />}
+                                />
+                              </Route>
                             </Route>
 
                             <Route path="setting">
