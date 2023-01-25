ed774f21f3929f432cf3758be3a941afef03dc24
ecoknows <jerico.villaraza19@gmail.com>
2023-01-25 15:38:46 +0800

    :bug: change category alignment and remove icon

--- src/features/see-me/presentation/components/see-me-tab-faq.tsx
+++ src/features/see-me/presentation/components/see-me-tab-faq.tsx
@@ -16,40 +16,33 @@ interface SeeMeTabFaqProps {
 
 export function SeeMeTabFaq(props: SeeMeTabFaqProps) {
   return (
-    <div className="flex flex-col items-start justify-start">
-      <ul className="flex flex-col pb-2 overflow-hidden text-white lg:flex-row lg:pb-0">
-        {props.tabs.map((tab, i) => (
-          <li
-            key={i}
-            className="flex items-end justify-end "
-            style={{ borderTopLeftRadius: "0.4375rem" }}
-          >
-            <Link
-              to={tab.url}
-              className={`${
-                props.activeTab === tab.active
-                  ? "profile-tab-active lg:bg-secondary bg-primary text-tertiary "
-                  : "text-white bg-primary"
-              } flex w-full font-semibold active space-x-2 items-center  rounded-t-md text-base text-start py-2 lg:py-4 lg:px-4 bg-paper`}
+    <>
+      <div className="overflow-auto ">
+        <ul className="flex pb-2 space-x-8 w-[600px]  text-white lg:pb-0">
+          {props.tabs.map((tab, i) => (
+            <li
+              key={i}
+              className="flex items-end justify-end "
+              style={{ borderTopLeftRadius: "0.4375rem" }}
             >
-              <img
-                src={`${REACT_APP_DOMAIN_URL}api/assets/images/shared/icons/${
-                  tab.active
-                }${props.activeTab === tab.active ? "-active" : ""}.png`}
-                className="w-[28px]"
-                alt="Entertainment Snacks est. 1994."
-              />{" "}
-              <span>{tab.name}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+              <Link
+                to={tab.url}
+                className={`${
+                  props.activeTab === tab.active
+                    ? "profile-tab-active lg:bg-secondary bg-primary text-tertiary "
+                    : "text-white bg-primary"
+                } flex w-full font-semibold active space-x-2 items-center  rounded-t-md text-xl text-start py-2 lg:py-4 lg:px-4 bg-paper`}
+              >
+                <span>{tab.name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <div className="w-full mb-10 md:rounded-tr-md md:border-0 profile-tab-content ">
         {props.children}
       </div>
-    </div>
+    </>
   );
 }
-
-// border-x-2 border-secondary
