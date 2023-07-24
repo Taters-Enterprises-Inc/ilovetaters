cce529d4d51beecbca1340e953f1da3122f124cb
knhrvs <knhrvs.dev@gmail.com>
2023-07-24 12:06:32 +0800

    new group added

--- src/features/stock-ordering/presentation/modals/supplier-update-billing.modal.tsx
+++ src/features/stock-ordering/presentation/modals/supplier-update-billing.modal.tsx
@@ -65,7 +65,7 @@ export function SupplierUpdateBillingModal(
     let result = false;
 
     user?.map((user_group) => {
-      if (user_group.id === 4 || user_group.id === 6) {
+      if (user_group.id === 4 || user_group.id === 6 || user_group.id === 10) {
         result = true;
       }
     });
