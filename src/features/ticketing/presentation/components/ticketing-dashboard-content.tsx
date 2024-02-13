55495e02974d2730838e2252c16d3f29817f4884
Christian Dale <114800018+saberdale03@users.noreply.github.com>
2024-02-13 19:13:29 +0800

    Full Revamp of Ticketing Dashboard Content
    Referenced from admin-user-discount. I changed it to more appropriate table for functions like search and sorting. Added responsiveness for smaller devices. Next Upadate: Triage Modal

--- src/features/ticketing/presentation/components/ticketing-dashboard-content.tsx
+++ src/features/ticketing/presentation/components/ticketing-dashboard-content.tsx
@@ -1,109 +1,150 @@
+// This code referenced from admin-user-discounts.tsx
+import { DataList, DataTable } from "features/shared/presentation/components";
 import {
-  Divider,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+  Column,
+  DataTableCell,
+  DataTableRow,
+} from "features/shared/presentation/components/data-table";
+import { ChangeEvent } from "react";
+import { FaEye } from "react-icons/fa";
+import { TbCheckupList } from "react-icons/tb";
+
+const columns: Array<Column> = [
+  { id: "status", label: "Status" },
+  { id: "ticketNumber", label: "Ticket Number" },
+  { id: "ticketTitle", label: "Ticket Title" },
+  { id: "dateCreated", label: "Date Created" },
+  { id: "actions", label: "Actions" },
+];
 
 export function TicketingDashboardContents() {
   return (
     <>
-      <div className="flex flex-col space-y-5">
-        <div>
-          <span className="text-secondary text-3xl font-['Bebas_Neue'] flex-1">
-            Dashboard
-          </span>
-        </div>
-
-        <div className="rounded-lg">
-          <div className="flex flex-col border border-gray-200 rounded-md shadow-sm bg-white p-5 space-y-3 w-full md:w-3/3">
-            <span className="text-secondary text-3xl font-['Bebas_Neue'] flex">
-              All Tickets
-            </span>
-            <Divider />
-
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell sx={{ fontWeight: 600 }}>Ticket#</TableCell>
-                  <TableCell sx={{ fontWeight: 600 }}>Title</TableCell>
-                  <TableCell sx={{ fontWeight: 600 }}>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {/* Row 1 */}
-                <TableRow
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                  }}
-                  className="w-full"
-                >
-                  <TableCell sx={{ fontSize: 14 }}>1</TableCell>
-                  <TableCell sx={{ fontSize: 14 }}>
-                    My PC isn't working
-                  </TableCell>
-
-                  <TableCell sx={{ fontSize: 14 }}>
-                    <Link to="/view-ticket" style={{ color: "blue" }}>
-                      View
-                    </Link>{" "}
-                    |{" "}
-                    <Link to="/triage-ticket" style={{ color: "blue" }}>
-                      Triage
-                    </Link>
-                  </TableCell>
-                </TableRow>
-
-                {/* Row 2 */}
-                <TableRow
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                  }}
-                  className="w-full"
-                >
-                  <TableCell sx={{ fontSize: 14 }}>2</TableCell>
-                  <TableCell sx={{ fontSize: 14 }}>
-                    My Mouse isn't working
-                  </TableCell>
-
-                  <TableCell sx={{ fontSize: 14 }}>
-                    <Link to="/view-ticket" style={{ color: "blue" }}>
-                      View
-                    </Link>{" "}
-                    |{" "}
-                    <Link to="/triage-ticket" style={{ color: "blue" }}>
-                      Triage
-                    </Link>
-                  </TableCell>
-                </TableRow>
-
-                {/* Row 3 */}
-                <TableRow
-                  sx={{
-                    "&:last-child td, &:last-child th": { border: 0 },
-                  }}
-                  className="w-full"
-                >
-                  <TableCell sx={{ fontSize: 14 }}>3</TableCell>
-                  <TableCell sx={{ fontSize: 14 }}>Taters Logo</TableCell>
-                  <TableCell sx={{ fontSize: 14 }}>
-                    <Link to="/view-ticket" style={{ color: "blue" }}>
-                      View
-                    </Link>{" "}
-                    |{" "}
-                    <Link to="/triage-ticket" style={{ color: "blue" }}>
-                      Triage
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </div>
-        </div>
+      <div className="flex flex-col lg:flex-row lg:items-end">
+        <span className="px-4 text-secondary text-3xl font-['Bebas_Neue'] flex-1">
+          All Tickets
+        </span>
+        {/* Add Chips Here */}
       </div>
+
+      <>
+        <div className="p-4 lg:hidden">
+          <DataList
+            search={""} // 👈  Edit This
+            emptyMessage="No tickets yet."
+            // 🔴 onSearch Not yet functional
+            onSearch={function (value: string): void {
+              throw new Error("Function not implemented.");
+            }}
+            // 🔴 onRowsPerPageChange Not yet functional
+            onRowsPerPageChange={function (
+              event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+            // 🔴 onPageChange Not yet functional
+            onPageChange={function (
+              event: ChangeEvent<unknown>,
+              value: number
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+            totalRows={0} // 👈 Edit this
+            perPage={0} // 👈 Edit this
+            page={0} // 👈 Edit this
+          >
+            <hr className="mt-4" />
+            <div className="flex flex-col px-4 py-2 border-b">
+              <span className="flex flex-wrap items-center space-x-1 text-xl">
+                <span>My PC Isn't Working</span>
+                <span
+                  className="px-2 py-1 text-xs rounded-full"
+                  style={{
+                    color: "white",
+                    backgroundColor: "#cca300",
+                  }}
+                >
+                  Open
+                </span>
+              </span>
+              <span className="text-xs text-gray-600">
+                <strong>Ticket Number:</strong> 159
+              </span>
+              <span className="text-xs text-gray-600">
+                <strong>Date Created:</strong> February 13, 2024
+              </span>
+            </div>
+          </DataList>
+        </div>
+        <div className="hidden p-4 lg:block">
+          <DataTable
+            order={"asc" ? "asc" : "desc"} // 👈  Edit This
+            orderBy={"dateCreated"} // 👈  Edit This
+            emptyMessage="No tickets yet."
+            search={""} // 👈  Edit This
+            // 🔴 onSearch Not yet functional
+            onSearch={function (value: string): void {
+              throw new Error("Function not implemented.");
+            }}
+            // 🔴 onRequestSort Not yet functional
+            onRequestSort={function (property: string): void {
+              throw new Error("Function not implemented.");
+            }}
+            columns={columns}
+            // 🔴 onRowsPerPageChange Not yet functional
+            onRowsPerPageChange={function (
+              event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+            // 🔴 onPageChange Not yet functional
+            onPageChange={function (
+              event: ChangeEvent<unknown>,
+              value: number
+            ): void {
+              throw new Error("Function not implemented.");
+            }}
+            totalRows={0} // 👈 Edit this
+            perPage={0} // 👈 Edit this
+            page={0} // 👈 Edit this
+          >
+            <DataTableRow key={0}>
+              <DataTableCell>
+                <span
+                  className="px-2 py-1 text-xs rounded-full"
+                  style={{
+                    color: "white",
+                    backgroundColor: "#cca300",
+                  }}
+                >
+                  {" "}
+                  Open{" "}
+                </span>
+              </DataTableCell>
+              <DataTableCell>159</DataTableCell>
+              <DataTableCell>My PC Isn't Working</DataTableCell>
+              <DataTableCell>February 13, 2024</DataTableCell>
+              <DataTableCell>
+                <button
+                  onClick={() => {
+                    alert("Button Not Yet Working");
+                  }}
+                  style={{ marginRight: "10px" }}
+                >
+                  <FaEye className="text-lg" />
+                </button>
+                <button
+                  onClick={() => {
+                    alert("Button Not Yet Working");
+                  }}
+                >
+                  <TbCheckupList className="text-lg" />
+                </button>
+              </DataTableCell>
+            </DataTableRow>
+          </DataTable>
+        </div>
+      </>
     </>
   );
 }
