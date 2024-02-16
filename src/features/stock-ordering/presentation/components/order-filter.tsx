ccc1d3b620419495d602ee6dc60e381aa433bc6a
knhrvs <knhrvs.dev@gmail.com>
2024-02-16 22:55:21 +0800

    Material management store filter with search

--- src/features/stock-ordering/presentation/components/order-filter.tsx
+++ src/features/stock-ordering/presentation/components/order-filter.tsx
@@ -1,15 +1,48 @@
-import { Button, Divider, Popover, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Card,
+  CardHeader,
+  Checkbox,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Popover,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdminSessionModel } from "features/admin/core/domain/admin-session.model";
 import {
   getAdminSession,
   selectGetAdminSession,
 } from "features/admin/presentation/slices/get-admin-session.slice";
 import { useAppSelector } from "features/config/hooks";
+import { theme } from "features/shared/constants";
 import {
   MaterialDateInput,
   MaterialInputAutoComplete,
 } from "features/shared/presentation/components";
-import { useState } from "react";
+import { set } from "lodash";
+import { useEffect, useState } from "react";
+
+import { FaChevronDown } from "react-icons/fa";
+
+function not(a: readonly StoreArray[], b: readonly StoreArray[]) {
+  return a.filter((value) => b.indexOf(value) === -1);
+}
+
+function intersection(a: readonly StoreArray[], b: readonly StoreArray[]) {
+  return a.filter((value) => b.indexOf(value) !== -1);
+}
+
+function union(a: readonly StoreArray[], b: readonly StoreArray[]) {
+  return [...a, ...not(b, a)];
+}
 
 interface filterData {
   store?: string | null;
@@ -18,24 +51,50 @@ interface filterData {
   end?: string | null;
 }
 
+interface ListOfActiveStores {
+  store_id: number;
+  name: string;
+  menu_name: string;
+}
+
 interface OrderFilterProps {
   anchor: HTMLButtonElement | null;
   onClose: () => void;
-  //   filter.store: (data: string) => void;
-  //   filter: (data: filterData) => void;
   filter: (data: filterData | string) => void;
 }
+
+interface StoreArray extends Array<ListOfActiveStores> {}
 
 export function OrderFilter(props: OrderFilterProps) {
   const getAdminSessionState = useAppSelector(selectGetAdminSession);
 
   const [filter, setfilter] = useState<filterData | null>(null);
+  const [checked, setChecked] = useState<readonly StoreArray[]>([]);
+  const [storeList, setStoreList] = useState<readonly StoreArray[]>([]);
+
+  const stores = getAdminSessionState.data?.admin.user_details.stores;
+
   const dateOption = [
     { id: 1, text: "Order placement date", name: "order_placement_date" },
     { id: 2, text: "Requested delivery date", name: "requested_delivery_date" },
     { id: 3, text: "Commited delivery date", name: "commited_delivery_date" },
     { id: 4, text: "Actual delivery date", name: "actual_delivery_date" },
   ];
+
+  useEffect(() => {
+    if (storeList.length === 0) {
+      const storeListData: StoreArray[] =
+        getAdminSessionState.data?.admin.user_details.stores.map((row) => [
+          {
+            store_id: row.store_id,
+            name: row.name,
+            menu_name: row.menu_name,
+          },
+        ]) ?? [];
+
+      setStoreList(storeListData);
+    }
+  }, [props.anchor]);
 
   const handleFilter = () => {
     if (filter?.store !== null) {
@@ -49,6 +108,8 @@ export function OrderFilter(props: OrderFilterProps) {
     ) {
       props.filter(filter ?? "");
     }
+
+    props.onClose();
   };
 
   const handleResetFilter = () => {
@@ -60,6 +121,50 @@ export function OrderFilter(props: OrderFilterProps) {
       end: null,
     }));
     props.filter({ store: null, type: null, start: null, end: null });
+
+    props.onClose();
+  };
+
+  const handleStoreSearch = (event: { target: { value: string } }) => {
+    const storeListData: StoreArray[] =
+      getAdminSessionState.data?.admin.user_details.stores.map((row) => [
+        {
+          store_id: row.store_id,
+          name: row.name,
+          menu_name: row.menu_name,
+        },
+      ]) ?? [];
+
+    const searchTerm = event.target.value.toLowerCase();
+    const filtered = storeList.map((store) =>
+      store.filter((row) => row.name.toLowerCase().includes(searchTerm))
+    );
+
+    setStoreList(searchTerm ? filtered : storeListData);
+  };
+
+  const numberOfChecked = (items: readonly StoreArray[]) =>
+    intersection(checked, items).length;
+
+  const handleSelectAllStore = (items: readonly StoreArray[]) => () => {
+    if (numberOfChecked(items) === items.length) {
+      setChecked(not(checked, items));
+    } else {
+      setChecked(union(checked, items));
+    }
+  };
+
+  const handleSelectStore = (value: StoreArray) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
   };
 
   return (
@@ -76,14 +181,13 @@ export function OrderFilter(props: OrderFilterProps) {
         horizontal: "left",
       }}
       PaperProps={{
-        style: {
-          padding: 10,
-          marginTop: 5,
-          backgroundColor: "white",
-          borderRadius: 5,
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-          width: "md",
+        elevation: 4,
+        sx: {
+          padding: 2,
+          width: { sm: "100%", md: "50%", lg: "25%" },
+          overflow: "auto",
         },
+        style: { maxHeight: "50vh" },
       }}
     >
       <div className="space-y-3">
@@ -98,102 +202,193 @@ export function OrderFilter(props: OrderFilterProps) {
             </Button>
           </div>
         </div>
-        <div className="flex-1 space-y-3">
-          <div className="flex-1 space-y-1">
-            <MaterialInputAutoComplete
-              placeholder="Filter by store"
-              colorTheme={"black"}
-              fullWidth
-              size="small"
-              options={
-                getAdminSessionState.data?.admin.user_details.stores ?? []
-              }
-              getOptionLabel={(option) => option.name || ""}
-              isOptionEqualToValue={(option, value) =>
-                option.name === value.name
-              }
-              onChange={(event, value) => {
-                if (value) {
-                  setfilter({
-                    ...filter,
-                    store: value.name ?? null,
-                  });
-                } else {
-                  setfilter({
-                    ...filter,
-                    store: null,
-                  });
-                }
-              }}
-            />
-          </div>
-          <Divider />
-          <div className="space-y-1">
-            <div>
-              <MaterialInputAutoComplete
-                placeholder="Date type"
+
+        <div>
+          <Accordion elevation={0}>
+            <AccordionSummary
+              expandIcon={<FaChevronDown />}
+              aria-controls="store-panel-content"
+              id="store-panel-header"
+            >
+              <Typography>Store</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Card elevation={0}>
+                <CardHeader
+                  avatar={
+                    <Checkbox
+                      onClick={handleSelectAllStore(
+                        stores as unknown as readonly StoreArray[]
+                      )}
+                      checked={
+                        numberOfChecked(
+                          stores as unknown as readonly StoreArray[]
+                        ) === stores?.length && stores?.length !== 0
+                      }
+                      indeterminate={
+                        numberOfChecked(
+                          stores as unknown as readonly StoreArray[]
+                        ) !== stores?.length &&
+                        numberOfChecked(
+                          stores as unknown as readonly StoreArray[]
+                        ) !== 0
+                      } //if not equal to length and not equal to zer0
+                      inputProps={{
+                        "aria-label": "all items selected",
+                      }}
+                    />
+                  }
+                  title={"List of active stores"}
+                  subheader={`${numberOfChecked(
+                    stores as unknown as readonly StoreArray[]
+                  )}/${stores?.length} selected`}
+                />
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  placeholder="Search store"
+                  size="small"
+                  onChange={handleStoreSearch}
+                  name={"searchStore"}
+                />
+                <List dense component="div" role="list">
+                  {storeList.map((value: StoreArray, index) => {
+                    const labelId = `store-list-item-${value}-label`;
+                    return (
+                      <>
+                        <ListItemButton
+                          key={index}
+                          role="listitem"
+                          onClick={handleSelectStore(value)}
+                        >
+                          <ListItemIcon>
+                            <Checkbox
+                              checked={checked.indexOf(value) !== -1}
+                              tabIndex={-1}
+                              disableRipple
+                              inputProps={{
+                                "aria-labelledby": labelId,
+                              }}
+                            />
+                          </ListItemIcon>
+                          <ListItemText
+                            id={labelId}
+                            primary={value.map((row) => row.name)}
+                          />
+                        </ListItemButton>
+                        <Divider variant="middle" />
+                      </>
+                    );
+                  })}
+                </List>
+              </Card>
+
+              {/* <MaterialInputAutoComplete
+                placeholder="Filter by store"
                 colorTheme={"black"}
                 fullWidth
                 size="small"
-                options={dateOption ?? []}
-                getOptionLabel={(option) => option.text || ""}
+                options={
+                  stores.stores ?? []
+                }
+                getOptionLabel={(option) => option.name || ""}
                 isOptionEqualToValue={(option, value) =>
-                  option.text === value.text
+                  option.name === value.name
                 }
                 onChange={(event, value) => {
                   if (value) {
                     setfilter({
                       ...filter,
-                      type: value.name ?? null,
+                      store: value.name ?? null,
                     });
                   } else {
                     setfilter({
                       ...filter,
-                      type: null,
-                      start: null,
-                      end: null,
+                      store: null,
                     });
                   }
                 }}
-              />
-            </div>
-            <div className="flex space-x-2">
-              <div className="flex-1 space-y-1">
-                <span>Start Date</span>
-                <MaterialDateInput
-                  value={filter?.start ?? null}
-                  size="small"
-                  colorTheme={"black"}
-                  openTo="year"
-                  views={["year", "month", "day"]}
-                  disableFuture={false}
-                  onChange={(value: any) => {
-                    setfilter({
-                      ...filter,
-                      start: value ?? null,
-                    });
-                  }}
-                />
-              </div>
+              /> */}
+            </AccordionDetails>
+          </Accordion>
+          <Divider />
+          <Accordion elevation={0}>
+            <AccordionSummary
+              expandIcon={<FaChevronDown />}
+              aria-controls="Date-panel-content"
+              id="Date-panel-header"
+            >
+              <Typography>Date range</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="space-y-1">
+                <div>
+                  <MaterialInputAutoComplete
+                    placeholder="Date type"
+                    colorTheme={"black"}
+                    fullWidth
+                    size="small"
+                    options={dateOption ?? []}
+                    getOptionLabel={(option) => option.text || ""}
+                    isOptionEqualToValue={(option, value) =>
+                      option.text === value.text
+                    }
+                    onChange={(event, value) => {
+                      if (value) {
+                        setfilter({
+                          ...filter,
+                          type: value.name ?? null,
+                        });
+                      } else {
+                        setfilter({
+                          ...filter,
+                          type: null,
+                          start: null,
+                          end: null,
+                        });
+                      }
+                    }}
+                  />
+                </div>
+                <div className="flex space-x-2">
+                  <div className="flex-1 space-y-1">
+                    <span>Start Date</span>
+                    <MaterialDateInput
+                      value={filter?.start ?? null}
+                      size="small"
+                      colorTheme={"black"}
+                      openTo="year"
+                      views={["year", "month", "day"]}
+                      disableFuture={false}
+                      onChange={(value: any) => {
+                        setfilter({
+                          ...filter,
+                          start: value ?? null,
+                        });
+                      }}
+                    />
+                  </div>
 
-              <div className="flex-1 space-y-1">
-                <span>End Date</span>
-                <MaterialDateInput
-                  value={filter?.end ?? null}
-                  size="small"
-                  colorTheme={"black"}
-                  openTo="year"
-                  views={["year", "month", "day"]}
-                  onChange={(value: any) => {
-                    setfilter({
-                      ...filter,
-                      end: value ?? null,
-                    });
-                  }}
-                />
+                  <div className="flex-1 space-y-1">
+                    <span>End Date</span>
+                    <MaterialDateInput
+                      value={filter?.end ?? null}
+                      size="small"
+                      colorTheme={"black"}
+                      openTo="year"
+                      views={["year", "month", "day"]}
+                      onChange={(value: any) => {
+                        setfilter({
+                          ...filter,
+                          end: value ?? null,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </AccordionDetails>
+          </Accordion>
         </div>
       </div>
     </Popover>
