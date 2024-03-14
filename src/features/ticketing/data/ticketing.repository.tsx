b8454f50f2edbeb3d0018d5e1b6bdd3b04d86e7f
Christian Dale <114800018+saberdale03@users.noreply.github.com>
2024-03-14 11:45:19 +0800

    Update GetMyTicketsRepository endpoint (NOT FINAL, fetching all-tickets endpoint)

--- src/features/ticketing/data/ticketing.repository.tsx
+++ src/features/ticketing/data/ticketing.repository.tsx
@@ -9,7 +9,23 @@ export interface GetAllTicketsResponse {
   };
 }
 
+export interface GetMyTicketsResponse {
+  data: {
+    message: string;
+    data: GetAllTicketsModel;
+  };
+}
+
 export function GetAllTicketsRepository(
+  query: string
+): Promise<GetAllTicketsResponse> {
+  return axios.get(`${REACT_APP_DOMAIN_URL}api/ticketing/tickets${query}`, {
+    withCredentials: true,
+  });
+}
+
+// 👇 Edit this axios call to match the actual API endpoint, the current one is for testing MyTickets only. 👇
+export function GetMyTicketsRepository(
   query: string
 ): Promise<GetAllTicketsResponse> {
   return axios.get(`${REACT_APP_DOMAIN_URL}api/ticketing/tickets${query}`, {
